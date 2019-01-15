7bd3f55dc81b202d79abcee905ff6aa64866fbb3
Cesar Bernal <cesarbworkmail@gmail.com>
2019-01-15 21:31:07 +0000

    tests for new project

--- app1.py
+++ app1.py
@@ -13,22 +13,22 @@ mongo = PyMongo(app)
 
 ''' Brings all the tasks to the main page '''
 @app.route('/')
-@app.route('/get_tasks')
+@app.route('/test_get_tasks')
 def get_tasks():
-    return render_template("tasks.html", 
-    tasks=mongo.db.tasks.find())
+    return render_template("t_tasks.html", 
+    nests=mongo.db.nesting.find())
     
 ''' brings the categories to addtask.html '''
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html',
+    return render_template('t_addtask.html',
     categories=mongo.db.categories.find())
     
     
 ''' inserts one dictionary when the form in addtask.html is submited '''
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks =  mongo.db.tasks
+    tasks =  mongo.db.nesting
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
@@ -36,39 +36,36 @@ def insert_task():
 ''' brings all the information to the form in edittask.html '''
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    the_task =  mongo.db.nesting.find_one({"_id": ObjectId(task_id)})
     all_categories =  mongo.db.categories.find()
-    return render_template('edittask.html', task=the_task, categories=all_categories)
+    return render_template('t_edittask.html', task=the_task, categories=all_categories)
     
     
 ''' updates all the fields in the entry after form is submited '''
 @app.route('/update_task/<task_id>', methods=["POST"])
 def update_task(task_id):
-    tasks = mongo.db.tasks
-    tasks.update( {'_id': ObjectId(task_id)},
-    {
+    tasks = mongo.db.nesting
+    tasks.update( {'_id': ObjectId(task_id)},{
         'task_name':request.form.get['task_name'],
         'category_name':request.form.get['category_name'],
         'task_description': request.form.get['task_description'],
         'due_date': request.form.get['due_date'],
-        'is_urgent':request.form.get['is_urgent'],
-        'Alergen': [
-            {'diary': request.form.get['diary']}
-            ]
+        'is_urgent':request.form.get['is_urgent']
     })
+    tasks.update_one({'_id': ObjectId(task_id)},{'$set':{"alergen":[{"diary":request.form.get['diary']}]}})
     return redirect(url_for('get_tasks'))
     
     
 @app.route('/delete_task/<task_id>')
 def delete_task(task_id):
-    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    mongo.db.nesting.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
     
 ''' displays all categories menu   '''
 
 @app.route('/get_categories')
 def get_categories():
-    return render_template('categories.html',
+    return render_template('t_categories.html',
     categories=mongo.db.categories.find())
     
 ''' link to edit button, brigs a new page with a form to 
@@ -76,7 +73,7 @@ edit the chosen category '''
 
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
-    return render_template('editcategory.html',
+    return render_template('t_editcategory.html',
     category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
 
 ''' this function is a form action for edit category  '''
@@ -105,12 +102,12 @@ def insert_category():
 
 @app.route('/new_category')
 def new_category():
-    return render_template('addcategory.html')
+    return render_template('t_addcategory.html')
     
     
 '''TESTING'''
 
-@app.route('/test')
+@app.route('/addtest')
 def test():
     return render_template('test.html',
     categories=mongo.db.categories.find())
@@ -138,7 +135,7 @@ def update_nesting(task_id):
             ]
     })
     return redirect(url_for('get_tasks'))
-    
+   
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
